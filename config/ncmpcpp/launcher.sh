74a9589889a151e2528dcf175696623996fdff38
fclfernando <fc4419039@gmail.com>
2026-06-29 15:34:37 -0600

    fix: sync ncmpcpp launcher.sh from live

--- config/ncmpcpp/launcher.sh
+++ config/ncmpcpp/launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Lanza kitty con split horizontal: ncmpcpp + carátula automática
 
-kitty --session "$HOME/.config/ncmpcpp/session.conf" \
+kitty --session /home/spectre/.config/ncmpcpp/session.conf \
       --class "music" \
       --title "ncmpcpp" \
-      --config "$HOME/.config/ncmpcpp/kitty-music.conf"
+      --config /home/spectre/.config/ncmpcpp/kitty-music.conf
