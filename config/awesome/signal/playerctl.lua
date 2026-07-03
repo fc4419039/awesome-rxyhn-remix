f934558d36536f7e0f2f5d9a0db64c4e1307aa94
fclfernando <fc4419039@gmail.com>
2026-07-03 12:42:45 -0600

    fix: playerctl notify skip Firefox (evita spam al hoverear videos)
    Cada vez que pasas el mouse sobre un video en YouTube,
    Firefox actualiza metadata MPRIS y disparaba una notificacion
    de 'reproduciendo'. Web notifications (Gmail, WhatsApp, etc.)
    no se ven afectadas.

--- config/awesome/signal/playerctl.lua
+++ config/awesome/signal/playerctl.lua
@@ -4,7 +4,7 @@ local bling = require("module.bling")
 local playerctl = bling.signal.playerctl.lib()
 
 playerctl:connect_signal("metadata", function(_, title, artist, album_path, album, new, player_name)
-    if new == true and title and title ~= "" then
+    if new == true and title and title ~= "" and player_name ~= "firefox" then
         naughty.notify ({
             app_name = 'Music',
             title = title,
